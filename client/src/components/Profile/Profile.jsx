165a6cb9d481c2a2f53c363957ef33dca7d252f3
MaximKraychev <maxim.kraychev@gmail.com>
2023-11-15 00:12:55 +0200

    Add PreviewPost component

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -1,11 +1,12 @@
 import styles from './Profile.module.css';
+import { PreviewPost } from './PreviewPost/PreviewPost';
 
 export const Profile = () => {
     return (
         <section className={styles['profile-section']}>
             <header>
                 <div className={styles['profile-picture-container']}>
-                        <img src="https://images.unsplash.com/photo-1698778755079-a76db5955d2c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIxfHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D" alt="avatar" />
+                    <img src="https://images.unsplash.com/photo-1698778755079-a76db5955d2c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIxfHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D" alt="avatar" />
                 </div>
                 <div className={styles['profile-details']}>
                     <p>username-goes-here</p>
@@ -14,30 +15,15 @@ export const Profile = () => {
             </header>
 
             <div className={styles['user-posts']}>
-                <div className={styles['post']}>
-                    <img src="https://plus.unsplash.com/premium_photo-1698952282280-c1fb6443092c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8" alt="image" />
-                </div>
-                <div className={styles['post']}>
-                    <img src="https://plus.unsplash.com/premium_photo-1698952282280-c1fb6443092c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8" alt="image" />
-                </div>
-                <div className={styles['post']}>
-                    <img src="https://plus.unsplash.com/premium_photo-1698952282280-c1fb6443092c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8" alt="image" />
-                </div>
-                <div className={styles['post']}>
-                    <img src="https://plus.unsplash.com/premium_photo-1698952282280-c1fb6443092c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8" alt="image" />
-                </div>
-                <div className={styles['post']}>
-                    <img src="https://plus.unsplash.com/premium_photo-1698952282280-c1fb6443092c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8" alt="image" />
-                </div>
-                <div className={styles['post']}>
-                    <img src="https://plus.unsplash.com/premium_photo-1698952282280-c1fb6443092c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8" alt="image" />
-                </div>
-                <div className={styles['post']}>
-                    <img src="https://plus.unsplash.com/premium_photo-1698952282280-c1fb6443092c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8" alt="image" />
-                </div>
-                <div className={styles['post']}>
-                    <img src="https://plus.unsplash.com/premium_photo-1698952282280-c1fb6443092c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8" alt="image" />
-                </div>
+                <PreviewPost />
+                <PreviewPost />
+                <PreviewPost />
+                <PreviewPost />
+                <PreviewPost />
+                <PreviewPost />
+                <PreviewPost />
+                <PreviewPost />
+                <PreviewPost />
             </div>
         </section>
     );
