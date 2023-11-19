a67c17a3d83158af87794245f911a5bb69942d34
MaximKraychev <maxim.kraychev@gmail.com>
2023-11-19 18:11:24 +0200

    Add working base register (todo conn to server)

--- client/src/components/Auth/Register/Register.jsx
+++ client/src/components/Auth/Register/Register.jsx
@@ -1,15 +1,60 @@
 import { Link } from 'react-router-dom';
 import styles from './Register.module.css';
-import { PATH } from '../../../paths/paths';
+import { PATH } from '../../../constants/paths';
+import { INPUT_NAMES } from '../../../constants/formInputNaming';
+import { useForm } from '../../../hooks/useForm';
 
 export const Register = () => {
+
+    const { values, changeHandler, onSubmit } = useForm({
+        [INPUT_NAMES.EMAIL]: '',
+        [INPUT_NAMES.USERNAME]: '',
+        [INPUT_NAMES.PASSWORD]: '',
+        [INPUT_NAMES.REPASSWORD]: ''
+    },
+        onRegisterSubmit)
+
+    async function onRegisterSubmit(data) {
+        console.log(data);
+    }
+
     return (
         <div className={styles['form-container']}>
-            <form className={styles['login-form']}>
+            <form className={styles['login-form']} method='POST' onSubmit={onSubmit}>
                 <h2>Create Account</h2>
-                <input type="email" placeholder='Email' />
-                <input type="text" placeholder='Username' />
-                <input type="password" placeholder='Password' />
+
+                <input
+                    type="email"
+                    name={INPUT_NAMES.EMAIL}
+                    placeholder='Email'
+                    value={values[INPUT_NAMES.EMAIL]}
+                    onChange={changeHandler}
+                />
+
+                <input
+                    type="text"
+                    name={INPUT_NAMES.USERNAME}
+                    placeholder='Username'
+                    value={values[INPUT_NAMES.USERNAME]}
+                    onChange={changeHandler}
+                />
+
+                <input
+                    type="password"
+                    name={INPUT_NAMES.PASSWORD}
+                    placeholder='Password'
+                    value={values[INPUT_NAMES.PASSWORD]}
+                    onChange={changeHandler}
+                />
+
+                <input
+                    type="password"
+                    name={INPUT_NAMES.REPASSWORD}
+                    placeholder='Password'
+                    value={values[INPUT_NAMES.REPASSWORD]}
+                    onChange={changeHandler}
+                />
+
                 <input type="submit" value={'Sign up'} />
                 <p className={styles['option']}>
                     You have an account? <Link to={PATH.LOGIN}>Login here!</Link>
