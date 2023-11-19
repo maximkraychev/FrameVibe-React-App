c1e784c3f28608fcd9d25a1724d4dd8750dbe85e
MaximKraychev <maxim.kraychev@gmail.com>
2023-11-19 22:44:02 +0200

    Add working basic api for requests to server

---
+++ client/src/services/api.js
@@ -0,0 +1,57 @@
+const host = 'http://localhost:3000';
+
+async function request(type, path, data) {
+
+    const options = {
+        method: type,
+        headers: {}
+    }
+
+    if (data != undefined) {
+        options.headers['Content-Type'] = 'application/json';
+        options.body = JSON.stringify(data);
+    }
+
+    //TODO logic for sending token
+    // const token = getUserData()?.accessToken;
+    // if (token) {
+    //     options.headers['X-Authorization'] = token;
+    // }
+
+    try {
+        const response = await fetch(host + path, options);
+
+        if (response.ok == false) {
+            //TODO add logic for clearing local user state 
+            // if (response.status == 403) {
+            //     clearUserData();
+            // }
+
+            const error = await response.json();
+            throw new Error(error.message);
+        }
+
+        if (response.status == 204) {
+            return response;
+        } else {
+            return response.json();
+        }
+
+    } catch (err) {
+        //TODO handle the error;
+        alert(err.message);
+        // throw err;
+    }
+}
+
+const get = request.bind(null, 'GET');
+const post = request.bind(null, 'POST');
+const put = request.bind(null, 'PUT');
+const del = request.bind(null, 'DELETE');
+
+export {
+    get,
+    post,
+    put,
+    del as delete 
+}
