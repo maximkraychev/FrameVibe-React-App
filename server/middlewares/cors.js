f842d0f9df1e2020d04aa3b876d0756c986746fb
MaximKraychev <maxim.kraychev@gmail.com>
2023-11-20 16:37:14 +0200

    Add header and specify the localhost origin

--- server/middlewares/cors.js
+++ server/middlewares/cors.js
@@ -1,6 +1,6 @@
 export default () => (req, res, next) => {
     // Here we can add which host can use our server
-	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
 
 	res.setHeader(
 		'Access-Control-Allow-Methods',
@@ -11,6 +11,9 @@ export default () => (req, res, next) => {
 		'Access-Control-Allow-Headers',
 		'X-Requested-With, Content-Type, X-Authorization'
 	);
+
+	 // Allow credentials (cookies)
+	res.setHeader('Access-Control-Allow-Credentials', 'true');
 
 	next();
 };
