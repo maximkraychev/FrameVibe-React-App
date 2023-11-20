1ed6165199d1a2fbaa681f1ca75c83d5e354217d
MaximKraychev <maxim.kraychev@gmail.com>
2023-11-20 11:51:13 +0200

    Add feature for image preview when img is uploaded

--- client/src/components/UploadImage/UploadImage.jsx
+++ client/src/components/UploadImage/UploadImage.jsx
@@ -3,23 +3,35 @@ import styles from './UploadImage.module.css';
 
 export const UploadImage = () => {
 
-    const [fileName, setFileName] = useState('');
+    const [previewImage, setPreviewImage] = useState(null);
 
+    const handleImageChange = (event) => {
+        const file = event.target.files[0];
 
-    function showSelectedFileName(e) {
-        setFileName(e.target.value);
-    }
+        if (file) {
+            const reader = new FileReader();
 
+            reader.onload = (e) => {
+                setPreviewImage(e.target.result);
+            };
+
+            reader.readAsDataURL(file);
+        }
+    };
 
     return (
         <div className={styles['form-container']}>
             <form className={styles['upload-image-form']}>
                 <h2>Image Upload</h2>
                 <label htmlFor="upload-image" className={styles['label-for-upload-image']}>Select Image</label>
-                <input type="file" name='uploadImage' id='upload-image' accept="image/png, image/jpeg" onChange={showSelectedFileName} />
-                {fileName &&
-                    <p className={styles['file-selected']}>{fileName}</p>
-                }
+                <input type="file" name='uploadImage' id='upload-image' accept="image/png, image/jpeg" onChange={handleImageChange} />
+                {previewImage && (
+                    <img
+                        className={styles['image-preview']}
+                        src={previewImage}
+                        alt="Preview"
+                    />
+                )}
                 <textarea name='description' rows="6" cols="50"></textarea>
                 <input type="submit" value={'Upload'} />
             </form>
