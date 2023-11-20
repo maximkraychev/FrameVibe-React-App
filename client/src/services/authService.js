d291ef1bbb37b14645a6331467d467a205e06827
MaximKraychev <maxim.kraychev@gmail.com>
2023-11-20 13:20:14 +0200

    Add loginService

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,5 +1,10 @@
+import { API_PATH } from '../constants/paths.js';
 import * as api from './api.js';
 
 export const registerService = async (data) => {
-    return api.post('/users/register', data);
+    return api.post(API_PATH.REGISTER, data);
+}
+
+export const loginService = async (data) => {
+    return api.post(API_PATH.LOGIN, data);
 }
