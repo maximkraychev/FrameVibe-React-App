f89b9eefb8d355e017b1412efcc2dd38cc3b246b
MaximKraychev <maxim.kraychev@gmail.com>
2023-11-22 19:06:03 +0200

    Add navigation after login

--- client/src/components/Auth/Login/Login.jsx
+++ client/src/components/Auth/Login/Login.jsx
@@ -1,12 +1,14 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import styles from './Login.module.css';
 import { PATH } from '../../../constants/paths';
 import { useForm } from '../../../hooks/useForm';
 import { INPUT_NAMES } from '../../../constants/formInputNaming';
 import { loginService } from '../../../services/authService';
 
+
 export const Login = () => {
 
+    const navigate = useNavigate();
     const { values, changeHandler, onSubmit } = useForm({
         [INPUT_NAMES.EMAIL]: '',
         [INPUT_NAMES.PASSWORD]: '',
@@ -14,9 +16,8 @@ export const Login = () => {
         onLoginSubmit)
 
     async function onLoginSubmit(data) {
-        console.log(data);
-        const result = await loginService(data);
-        console.log(result);
+        await loginService(data);
+        navigate(PATH.EXPLORE);
     }
 
     return (
