1efec23ddb9c65a9bce392e952408bb012bd6b19
MaximKraychev <maxim.kraychev@gmail.com>
2023-11-22 19:07:05 +0200

    TODO..UploadImage (find a way how to send the img)

--- client/src/components/UploadImage/UploadImage.jsx
+++ client/src/components/UploadImage/UploadImage.jsx
@@ -1,13 +1,22 @@
 import { useState } from 'react';
+
+import { useForm } from '../../hooks/useForm';
 import styles from './UploadImage.module.css';
+import { INPUT_NAMES } from '../../constants/formInputNaming';
 
 export const UploadImage = () => {
 
+    const { values, changeHandler, onSubmit } = useForm({
+        [INPUT_NAMES.UPLOAD_IMAGE]: '',
+        [INPUT_NAMES.DESCRIPTION]: ''
+    }, uploadImageSubmitHandler);
+    
     const [previewImage, setPreviewImage] = useState(null);
 
     const handleImageChange = (event) => {
+        changeHandler(event);
         const file = event.target.files[0];
-
+        console.log(event.target.files[0]);
         if (file) {
             const reader = new FileReader();
 
@@ -24,12 +33,16 @@ export const UploadImage = () => {
         }
     };
 
+    function uploadImageSubmitHandler(formData) {
+        console.log(formData);
+    }
+
     return (
         <div className={styles['form-container']}>
-            <form className={styles['upload-image-form']}>
+            <form className={styles['upload-image-form']} onSubmit={onSubmit}>
                 <h2>Image Upload</h2>
                 <label htmlFor="upload-image" className={styles['label-for-upload-image']}>Select Image</label>
-                <input type="file" name='uploadImage' id='upload-image' accept="image/png, image/jpeg" onChange={handleImageChange} />
+                <input type="file" name={INPUT_NAMES.UPLOAD_IMAGE} id='upload-image' accept="image/png, image/jpeg" value={values[INPUT_NAMES.UPLOAD_IMAGE]} onChange={handleImageChange} />
                 {previewImage && (
                     <img
                         className={styles['image-preview']}
@@ -37,7 +50,7 @@ export const UploadImage = () => {
                         alt="Preview"
                     />
                 )}
-                <textarea name='description' rows="6" cols="50"></textarea>
+                <textarea name={INPUT_NAMES.DESCRIPTION} value={values[INPUT_NAMES.DESCRIPTION]} onChange={changeHandler} rows="6" cols="50"></textarea>
                 <input type="submit" value={'Upload'} />
             </form>
         </div>
