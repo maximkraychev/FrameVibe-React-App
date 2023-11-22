1a87bd3490de8d1f504d198538681bc87662fe00
MaximKraychev <maxim.kraychev@gmail.com>
2023-11-22 19:05:13 +0200

    Add additional input names in constants

--- client/src/constants/formInputNaming.js
+++ client/src/constants/formInputNaming.js
@@ -2,5 +2,7 @@ export const INPUT_NAMES = {
     EMAIL: 'email',
     USERNAME: 'username',
     PASSWORD: 'password',
-    REPASSWORD: 'repassword'
+    REPASSWORD: 'repassword',
+    DESCRIPTION: 'description',
+    UPLOAD_IMAGE: 'uploadImage'
 }
