5221b853ba496ebfd0ffeb9640f9a70e8b30dc2f
MaximKraychev <maxim.kraychev@gmail.com>
2023-11-25 17:36:32 +0200

    Add useFormReducer hook into UploadImage component

--- client/src/components/UploadImage/UploadImage.jsx
+++ client/src/components/UploadImage/UploadImage.jsx
@@ -1,22 +1,21 @@
 import { useState } from 'react';
 
-import { useForm } from '../../hooks/useForm';
 import styles from './UploadImage.module.css';
 import { INPUT_NAMES } from '../../constants/formInputNaming';
+import { useFormReducer } from '../../hooks/useFormReducer';
+
+const initialFormState = {
+    [INPUT_NAMES.UPLOAD_IMAGE]: '',
+    [INPUT_NAMES.DESCRIPTION]: ''
+}
 
 export const UploadImage = () => {
-
-    const { values, changeHandler, onSubmit } = useForm({
-        [INPUT_NAMES.UPLOAD_IMAGE]: '',
-        [INPUT_NAMES.DESCRIPTION]: ''
-    }, uploadImageSubmitHandler);
-    
+    const { values, changeHandler, onSubmit } = useFormReducer(initialFormState, uploadImageSubmitHandler);
     const [previewImage, setPreviewImage] = useState(null);
 
     const handleImageChange = (event) => {
-        changeHandler(event);
+        changeHandler(event, 'IMAGE_FORM');
         const file = event.target.files[0];
-        console.log(event.target.files[0]);
         if (file) {
             const reader = new FileReader();
 
@@ -27,14 +26,15 @@ export const UploadImage = () => {
             reader.onerror = (e) => {
                 console.error('Error reading the file:', e.target.error);
                 // TODO add modal that shows the error message; 
-              };
+            };
 
             reader.readAsDataURL(file);
         }
     };
 
     function uploadImageSubmitHandler(formData) {
-        console.log(formData);
+            console.log(formData);
+        // console.log(previewImage);
     }
 
     return (
@@ -42,7 +42,7 @@ export const UploadImage = () => {
             <form className={styles['upload-image-form']} onSubmit={onSubmit}>
                 <h2>Image Upload</h2>
                 <label htmlFor="upload-image" className={styles['label-for-upload-image']}>Select Image</label>
-                <input type="file" name={INPUT_NAMES.UPLOAD_IMAGE} id='upload-image' accept="image/png, image/jpeg" value={values[INPUT_NAMES.UPLOAD_IMAGE]} onChange={handleImageChange} />
+                <input type="file" name={INPUT_NAMES.UPLOAD_IMAGE} id='upload-image' accept="image/png, image/jpeg" onChange={handleImageChange} />
                 {previewImage && (
                     <img
                         className={styles['image-preview']}
