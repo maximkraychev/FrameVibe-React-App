538c7da8e7794babac8d558e27dc97760530bc6e
MaximKraychev <maxim.kraychev@gmail.com>
2023-11-25 18:10:38 +0200

    Change export for cloudinary

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -5,11 +5,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const cloudinaryConfig = () => {
-    cloudinary.config({
-        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-        api_key: process.env.CLOUDINARY_API_KEY,
-        api_secret: process.env.CLOUDINARY_API_SECRET
-      });
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+  });
 }
 
-export const cloudinary = cloudinary;
+export { cloudinary };
