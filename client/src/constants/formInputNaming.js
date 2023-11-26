f9279d625d8859f41501eb9ea98d9a61db06a90d
MaximKraychev <maxim.kraychev@gmail.com>
2023-11-26 23:55:09 +0200

    Add additional properties for INPUT_NAMING

--- client/src/constants/formInputNaming.js
+++ client/src/constants/formInputNaming.js
@@ -4,5 +4,7 @@ export const INPUT_NAMES = {
     PASSWORD: 'password',
     REPASSWORD: 'repassword',
     DESCRIPTION: 'description',
-    UPLOAD_IMAGE: 'uploadImage'
+    UPLOAD_IMAGE: 'uploadImage',
+    USER_BASE_AVATAR_URL: 'https://res.cloudinary.com/framevibe/image/upload/v1700929694/ndzgp3bg1wz2pqs4ltob.png',
+    USER_AVATAR: 'avatar'
 }
