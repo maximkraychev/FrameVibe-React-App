a56dcc71fc1b4e3bd3ab1eed037095d51b0ec2dd
MaximKraychev <maxim.kraychev@gmail.com>
2023-11-26 23:54:34 +0200

    Add route in app component for logout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
 import { Route, Routes } from 'react-router-dom';
-import './global-css/global.css';
 
+import { AuthProvider } from './contexts/AuthContext';
 import { PATH } from './constants/paths';
+
+import './global-css/global.css';
 import { Header } from './components/Header/Header';
 import { Explore } from './components/Explore/Explore';
 import { Login } from './components/Auth/Login/Login';
@@ -10,21 +12,22 @@ import { UploadImage } from './components/UploadImage/UploadImage';
 import { Profile } from './components/Profile/Profile';
 import { Details } from './components/Details/Details';
 import { NotFound } from './components/NotFound/NotFound';
+import { Logout } from './components/Auth/Logout/Logout';
 
 function App() {
 
   return (
-    <>
+    <AuthProvider>
       <Header />
       <div className='section-container'>
         <Routes>
           <Route path='/' />
           <Route path={PATH.EXPLORE} element={<Explore />}>
-            <Route path={PATH.IMAGE_DETAILS} element={<Details />}/>
+            <Route path={PATH.IMAGE_DETAILS} element={<Details />} />
           </Route>
           <Route path={PATH.REGISTER} element={<Register />} />
           <Route path={PATH.LOGIN} element={<Login />} />
-          <Route path={PATH.LOGOUT} />
+          <Route path={PATH.LOGOUT} element={<Logout />}/>
           <Route path={PATH.UPLOAD} element={<UploadImage />} />
           <Route path={PATH.PROFILE} element={<Profile />}>
             <Route path={PATH.IMAGE_DETAILS} element={<Details />} />
@@ -32,7 +35,7 @@ function App() {
           <Route path={PATH.NOT_FOUND} element={<NotFound />} />
         </Routes>
       </div>
-    </>
+    </AuthProvider>
   );
 }
 
