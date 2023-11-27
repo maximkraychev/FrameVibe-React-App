549f38b8bb32c629e0c97005494f7a2972f56b34
MaximKraychev <maxim.kraychev@gmail.com>
2023-11-27 14:09:33 +0200

    Change base user avatar url

--- client/src/constants/formInputNaming.js
+++ client/src/constants/formInputNaming.js
@@ -5,6 +5,6 @@ export const INPUT_NAMES = {
     REPASSWORD: 'repassword',
     DESCRIPTION: 'description',
     UPLOAD_IMAGE: 'uploadImage',
-    USER_BASE_AVATAR_URL: 'https://res.cloudinary.com/framevibe/image/upload/v1700929694/ndzgp3bg1wz2pqs4ltob.png',
+    USER_BASE_AVATAR_URL: 'https://res.cloudinary.com/framevibe/image/upload/v1701085833/xjk2rcmagmbyxfuk9vfz.png',
     USER_AVATAR: 'avatar'
 }
