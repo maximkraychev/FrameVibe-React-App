ea5130ee48dfa894c72e13455d1455680df43ac0
MaximKraychev <maxim.kraychev@gmail.com>
2023-11-27 14:08:20 +0200

    Replace mock user avatar and username > real user

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -3,6 +3,7 @@ import { useContext } from 'react';
 
 import { PreviewPost } from './PreviewPost/PreviewPost';
 import { AuthContext } from '../../contexts/AuthContext';
+import { INPUT_NAMES } from '../../constants/formInputNaming';
 
 import styles from './Profile.module.css';
 
@@ -17,10 +18,10 @@ export const Profile = () => {
             <Outlet context={[urlAfterDetailsClose]}></Outlet>
             <header>
                 <div className={styles['profile-picture-container']}>
-                    <img src="https://images.unsplash.com/photo-1698778755079-a76db5955d2c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIxfHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D" alt="avatar" />
+                    <img src={auth[INPUT_NAMES.USER_AVATAR]} alt="avatar" />
                 </div>
                 <div className={styles['profile-details']}>
-                    <p>username-goes-here</p>
+                    <p>{auth.username}</p>
                     <button className={styles['profile-edit-btn']}>Edit profile</button>
                 </div>
             </header>
