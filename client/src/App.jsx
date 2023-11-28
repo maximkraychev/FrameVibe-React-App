4749eaee7461cb571631c3b81d005a58c4ba0a4e
MaximKraychev <maxim.kraychev@gmail.com>
2023-11-28 21:08:34 +0200

    Implement AuthGuard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import { Profile } from './components/Profile/Profile';
 import { Details } from './components/Details/Details';
 import { NotFound } from './components/NotFound/NotFound';
 import { Logout } from './components/Auth/Logout/Logout';
+import { AuthGuard } from './guards/AuthGuard';
 
 function App() {
 
@@ -22,16 +23,24 @@ function App() {
       <div className='section-container'>
         <Routes>
           <Route path='/' />
-          <Route path={PATH.EXPLORE} element={<Explore />}>
-            <Route path={PATH.IMAGE_DETAILS} element={<Details />} />
-          </Route>
           <Route path={PATH.REGISTER} element={<Register />} />
           <Route path={PATH.LOGIN} element={<Login />} />
-          <Route path={PATH.LOGOUT} element={<Logout />}/>
-          <Route path={PATH.UPLOAD} element={<UploadImage />} />
-          <Route path={PATH.PROFILE} element={<Profile />}>
-            <Route path={PATH.IMAGE_DETAILS} element={<Details />} />
+          <Route path={PATH.LOGOUT} element={<Logout />} />
+
+          <Route path={PATH.EXPLORE} element={<Explore />}>
+            <Route element={<AuthGuard />}>
+              <Route path={PATH.IMAGE_DETAILS} element={<Details />} />
+            </Route>
           </Route>
+
+          <Route element={<AuthGuard />}>
+            <Route path={PATH.UPLOAD} element={<UploadImage />} />
+            <Route path={PATH.PROFILE} element={<Profile />}>
+              <Route path={PATH.IMAGE_DETAILS} element={<Details />} />
+            </Route>
+
+          </Route>
+
           <Route path={PATH.NOT_FOUND} element={<NotFound />} />
         </Routes>
       </div>
