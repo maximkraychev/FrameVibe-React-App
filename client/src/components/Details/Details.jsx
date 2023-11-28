d7569e2899f5613177a92ee114b3d14c3b55ccae
MaximKraychev <maxim.kraychev@gmail.com>
2023-11-28 13:30:59 +0200

    Implement Modal component into Details component

--- client/src/components/Details/Details.jsx
+++ client/src/components/Details/Details.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
-import styles from './Details.module.css';
 import { useNavigate, useOutletContext } from 'react-router-dom';
+
+import styles from './Details.module.css';
 import { Xmark } from '../Svg/Xmark';
+import { Modal } from '../Modal/Modal';
 
 export const Details = () => {
     const navigate = useNavigate();
@@ -12,25 +14,25 @@ export const Details = () => {
     function changeVisibility() {
         setVisible((oldValue) => !oldValue);
         navigate(urlAfterClose);
+        // navigate(-1)  --> its not working if we come from a link directly to details
     }
 
     return (
         <>
             {isVisible &&
-                <div className={styles['dimmer']} onClick={changeVisibility}>
+                <Modal showHide={changeVisibility}>
                     <div className={styles['details-container']}>
-
                         <div className={styles['owner-mobile']}>
                             <div className={styles['avatar-container-mobile']}>
                                 <img src="https://images.unsplash.com/photo-1698778755079-a76db5955d2c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIxfHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D" alt="avatar" />
                             </div>
                             <p>Username</p>
-                            <Xmark onClick={changeVisibility}/>
+                            <Xmark onClick={changeVisibility} />
                         </div>
 
                         <div className={styles['image-container']}>
-                            <img src="https://plus.unsplash.com/premium_photo-1698952282280-c1fb6443092c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8" alt="main-image" />
-                            {/* <img src="https://images.unsplash.com/photo-1698778755357-63abc1fe986f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDMzfHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D" alt="main-image" /> */}
+                            {/* <img src="https://plus.unsplash.com/premium_photo-1698952282280-c1fb6443092c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8" alt="main-image" /> */}
+                            <img src="https://images.unsplash.com/photo-1698778755357-63abc1fe986f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDMzfHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D" alt="main-image" />
                         </div>
 
                         <div className={styles['description-container']}>
@@ -39,7 +41,7 @@ export const Details = () => {
                                     <img src="https://images.unsplash.com/photo-1698778755079-a76db5955d2c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIxfHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D" alt="avatar" />
                                 </div>
                                 <p>Username</p>
-                                <Xmark onClick={changeVisibility}/>
+                                <Xmark onClick={changeVisibility} />
                             </div>
                             <div className={styles['description']}>
                                 <p>
@@ -51,7 +53,7 @@ export const Details = () => {
 
                         </div>
                     </div>
-                </div>
+                </Modal>
             }
         </>
     );
