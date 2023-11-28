260f493ef9fb29a9b8a936318944b3bd5d5c105e
MaximKraychev <maxim.kraychev@gmail.com>
2023-11-28 21:45:49 +0200

    Replace magic string with PATH constant

--- client/src/components/NotFound/NotFound.jsx
+++ client/src/components/NotFound/NotFound.jsx
@@ -1,4 +1,6 @@
 import { Link } from 'react-router-dom';
+
+import { PATH } from '../../constants/paths';
 import styles from './NotFound.module.css';
 
 export const NotFound = () => {
@@ -10,7 +12,7 @@ export const NotFound = () => {
                     <h2>404</h2>
                     <h5>Oops! Page Not Be Found</h5>
                     <p>Sorry but the page you are looking for does not exist, have been removed. name changed or is temporarily unavailable</p>
-                    <Link to={'/'}>Back to homepage</Link>
+                    <Link to={PATH.EXPLORE}>Back to homepage</Link>
                 </div>
             </div>
         </div>
