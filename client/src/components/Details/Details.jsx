8aa7e25af7a730b22d7425c06d1bfd1bac46cc57
MaximKraychev <maxim.kraychev@gmail.com>
2023-11-29 18:06:08 +0200

    Change Details component to dummy(for now)

--- client/src/components/Details/Details.jsx
+++ client/src/components/Details/Details.jsx
@@ -1,59 +1,42 @@
-import { useContext } from 'react';
-
-import { useModalState } from '../../hooks/useModalState';
-import { DetailsContext } from '../../contexts/DetailsContext';
-
 import styles from './Details.module.css';
 import { Xmark } from '../Svg/Xmark';
-import { Modal } from '../Modal/Modal';
 
-export const Details = () => {
-    const { isVisible, reverseVisibilityState } = useModalState();
-    const handleUrlOnDetailsClose = useContext(DetailsContext);
-
-    function changeVisibility() {
-        reverseVisibilityState();
-        handleUrlOnDetailsClose();
-    }
+export const Details = (props) => {
 
     return (
         <>
-            {isVisible &&
-                <Modal showHide={changeVisibility}>
-                    <div className={styles['details-container']}>
-                        <div className={styles['owner-mobile']}>
-                            <div className={styles['avatar-container-mobile']}>
-                                <img src="https://images.unsplash.com/photo-1698778755079-a76db5955d2c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIxfHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D" alt="avatar" />
-                            </div>
-                            <p>Username</p>
-                            <Xmark onClick={changeVisibility} />
-                        </div>
-
-                        <div className={styles['image-container']}>
-                            {/* <img src="https://plus.unsplash.com/premium_photo-1698952282280-c1fb6443092c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8" alt="main-image" /> */}
-                            <img src="https://images.unsplash.com/photo-1698778755357-63abc1fe986f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDMzfHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D" alt="main-image" />
-                        </div>
-
-                        <div className={styles['description-container']}>
-                            <div className={styles['owner-desktop']}>
-                                <div className={styles['avatar-container-desktop']}>
-                                    <img src="https://images.unsplash.com/photo-1698778755079-a76db5955d2c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIxfHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D" alt="avatar" />
-                                </div>
-                                <p>Username</p>
-                                <Xmark onClick={changeVisibility} />
-                            </div>
-                            <div className={styles['description']}>
-                                <p>
-                                    It was fun thinking about how this should be viewed from the perspective of a component. It basically takes a window size and increases/decreases the pagination window on each click
-                                </p>
-                            </div>
-
-                            {/* <div className={styles['comments']}></div> */}
-
-                        </div>
+            <div className={styles['details-container']}>
+                <div className={styles['owner-mobile']}>
+                    <div className={styles['avatar-container-mobile']}>
+                        <img src="https://images.unsplash.com/photo-1698778755079-a76db5955d2c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIxfHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D" alt="avatar" />
                     </div>
-                </Modal>
-            }
+                    <p>Username</p>
+                    {props.changeVisibility && <Xmark onClick={changeVisibility} />}
+                </div>
+
+                <div className={styles['image-container']}>
+                    {/* <img src="https://plus.unsplash.com/premium_photo-1698952282280-c1fb6443092c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8" alt="main-image" /> */}
+                    <img src="https://images.unsplash.com/photo-1698778755357-63abc1fe986f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDMzfHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D" alt="main-image" />
+                </div>
+
+                <div className={styles['description-container']}>
+                    <div className={styles['owner-desktop']}>
+                        <div className={styles['avatar-container-desktop']}>
+                            <img src="https://images.unsplash.com/photo-1698778755079-a76db5955d2c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIxfHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D" alt="avatar" />
+                        </div>
+                        <p>Username</p>
+                        {props.changeVisibility && <Xmark onClick={changeVisibility} />}
+                    </div>
+                    <div className={styles['description']}>
+                        <p>
+                            It was fun thinking about how this should be viewed from the perspective of a component. It basically takes a window size and increases/decreases the pagination window on each click
+                        </p>
+                    </div>
+
+                    {/* <div className={styles['comments']}></div> */}
+
+                </div>
+            </div>
         </>
     );
 };
