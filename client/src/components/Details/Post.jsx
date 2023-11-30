f4fa3f976e5f36c1337d66ec19f98c7ec9903373
MaximKraychev <maxim.kraychev@gmail.com>
2023-11-30 14:15:58 +0200

    Change Post component

--- client/src/components/Details/Post.jsx
+++ client/src/components/Details/Post.jsx
@@ -1,13 +1,28 @@
-import { useLocation } from "react-router-dom";
+import { useFindBackgroundComponent } from "../../hooks/useFindBackgroundComponent";
+
 import styles from './Post.module.css';
 import { Details } from "./Details";
+import { Modal } from "../Modal/Modal";
 
 export const Post = () => {
-    const location = useLocation();
-    console.log(location.pathname);
+    const backgroundComponent = useFindBackgroundComponent(); // This is a reference to component;
+
     return (
-        <div className={styles['post-container']}>
-            <Details />
-        </div>
+        <>
+            {backgroundComponent &&
+                <>
+                    <Modal>
+                        <Details />
+                    </Modal>
+                    {backgroundComponent()}
+                </>
+            }
+
+            {!backgroundComponent &&
+                <div className={styles['post-container']}>
+                    <Details />
+                </div>
+            }
+        </>
     );
 }
