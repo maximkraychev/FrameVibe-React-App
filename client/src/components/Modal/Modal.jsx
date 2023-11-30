77b90329d185eaa34b87635370b34616d9421071
MaximKraychev <maxim.kraychev@gmail.com>
2023-11-30 14:13:47 +0200

    Add changes to Modal component

--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -1,10 +1,17 @@
+import { useNavigate } from 'react-router-dom';
 import styles from './Modal.module.css';
 
-export const Modal = ({children, ...props}) => {
+export const Modal = ({ children, ...props }) => {
+    const navigation = useNavigate();
+
+    function goBack() {
+        navigation(-1);
+    }
+
     return (
-        <div className={styles['dimmer']} onClick={props.showHide}>
+        <div className={styles['dimmer']} onClick={props.showHide ? props.showHide : goBack}>
             <div className={styles['modal-container']}>
-               {children}
+                {children}
             </div>
         </div>
     );
