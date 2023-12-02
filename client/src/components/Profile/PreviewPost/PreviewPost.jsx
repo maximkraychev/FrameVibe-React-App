d83caaded8dec4f8c91fe04bc2cd7bec6c5c9aa2
MaximKraychev <maxim.kraychev@gmail.com>
2023-12-02 22:09:44 +0200

    Add props for Preview component

--- client/src/components/Profile/PreviewPost/PreviewPost.jsx
+++ client/src/components/Profile/PreviewPost/PreviewPost.jsx
@@ -4,13 +4,13 @@ import { HeartSolidSvg } from '../../Svg/HeartSolid';
 
 //TODO... add the number of likes and comments 
 
-export const PreviewPost = () => {
+export const PreviewPost = ({_id, likes, imageURL}) => {
     return (
         <div className={styles['post']}>
-            <img src="https://plus.unsplash.com/premium_photo-1698952282280-c1fb6443092c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8" alt="image" />
+            <img src={imageURL} alt="image" />
             <div className={styles['svg-container']}>
                 <HeartSolidSvg />
-                <span>0</span> 
+                <span>{likes.length}</span> 
                 <CommentSvg />
                 <span>0</span>
             </div>
