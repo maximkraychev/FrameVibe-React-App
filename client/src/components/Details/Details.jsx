7489d247bccf75260479ad560e7304aa95b281a8
MaximKraychev <maxim.kraychev@gmail.com>
2023-12-03 19:56:35 +0200

    Add logic for Details component

--- client/src/components/Details/Details.jsx
+++ client/src/components/Details/Details.jsx
@@ -1,44 +1,83 @@
+import { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { StateContext } from '../../contexts/StateContext';
+import { getSinglePost } from '../../services/postService';
+import { getUserInfoByUsernameOrId } from '../../services/userService';
+import { PARAMS } from '../../constants/paths';
+import { STATE_FIELDS } from '../../constants/stateFieldsConstants';
+
 import styles from './Details.module.css';
 import { Xmark } from '../Svg/Xmark';
-import { useState } from 'react';
 
 export const Details = (props) => {
 
-    const [postState, setPostState] = useState(props.postState || {});
-    console.log(props);
+    const { state } = useContext(StateContext);
+    const [post, setPost] = useState({});
+    const [user, setUser] = useState({});
+    const params = useParams();
+
+    useEffect(() => {
+
+        try {
+            (async function getData () {
+                let currentPost = null;
+
+                if (state[STATE_FIELDS.POSTS]?.length !== 0) {
+                    currentPost = state[STATE_FIELDS.POSTS].find((post) => post._id == params[PARAMS.POSTID]);
+                }
+
+                if (!currentPost && params[PARAMS.POSTID]) {
+                    currentPost = await getSinglePost(params[PARAMS.POSTID]);
+                }
+
+                console.log(currentPost);
+                setPost(currentPost);
+
+                if (currentPost.owner) {
+                    const postOwner = await getUserInfoByUsernameOrId(currentPost.owner);
+                    setUser(postOwner);
+                }
+
+            })();
+
+        } catch (err) {
+            console.log(err);
+            //TODO handle the error
+        }
+
+    }, [])
 
     return (
         <>
             <div className={styles['details-container']}>
                 <div className={styles['owner-mobile']}>
                     <div className={styles['avatar-container-mobile']}>
-                        <img src={postState?.user?.avatar} alt="avatar" />
+                        <img src={user?.avatar} alt="avatar" />
                     </div>
-                    <p>{postState?.user?.username}</p>
+                    <p>{user?.username}</p>
                     {props.changeVisibility && <Xmark onClick={changeVisibility} />}
                 </div>
 
                 <div className={styles['image-container']}>
-                    {/* <img src="https://plus.unsplash.com/premium_photo-1698952282280-c1fb6443092c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8" alt="main-image" /> */}
-                    {/* <img src="https://images.unsplash.com/photo-1698778755357-63abc1fe986f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDMzfHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D" alt="main-image" /> */}
-                    <img src={postState?.post?.imageUrl} alt="main-image" />
+                    <img src={post?.imageURL} alt="main-image" />
                 </div>
 
                 <div className={styles['description-container']}>
                     <div className={styles['owner-desktop']}>
                         <div className={styles['avatar-container-desktop']}>
-                            <img src={postState?.user?.avatar} alt="avatar" />
+                            <img src={user?.avatar} alt="avatar" />
                         </div>
-                        <p>{postState?.user?.username}</p>
+                        <p>{user?.username}</p>
                         {props.changeVisibility && <Xmark onClick={changeVisibility} />}
                     </div>
                     <div className={styles['description']}>
                         <p>
-                           {postState?.post?.description}
+                            {post?.description}
                         </p>
                     </div>
 
-                    {/* <div className={styles['comments']}></div> */}
+                    {/* TODO..comments <div className={styles['comments']}></div> */}
 
                 </div>
             </div>
