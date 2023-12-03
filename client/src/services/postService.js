361a0a25d2587975d5cc37f340d83d877cde1ed6
MaximKraychev <maxim.kraychev@gmail.com>
2023-12-03 19:55:52 +0200

    Add service for single post

--- client/src/services/postService.js
+++ client/src/services/postService.js
@@ -8,3 +8,7 @@ export const createPost = async (data) => {
 export const getAllUserPosts = async (userId) => {
     return api.get(API_PATH.USER_POSTS_FN(userId));
 }
+
+export const getSinglePost = async (postId) => {
+    return api.get(API_PATH.SINGLE_POST_FN(postId));
+}
