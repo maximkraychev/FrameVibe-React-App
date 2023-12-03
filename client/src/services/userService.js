b9f6d116fe60ae4db4933dd514c43e4492478a77
MaximKraychev <maxim.kraychev@gmail.com>
2023-12-03 19:54:20 +0200

    Rename getUserInfo know it can search by id too

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,6 +1,6 @@
 import { API_PATH } from '../constants/paths.js';
 import * as api from './api.js';
 
-export const getUserInfo = async (username) => {
-    return api.get(API_PATH.USER_FN(username));
-}
+export const getUserInfoByUsernameOrId = async (userIdentifier) => {
+    return api.get(API_PATH.USER_FN(userIdentifier));
+};
