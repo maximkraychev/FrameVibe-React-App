9f0e6e06ef20c414751cc63077501846844aca77
MaximKraychev <maxim.kraychev@gmail.com>
2023-12-03 19:50:49 +0200

    Remove useFindBackgroundComponent hook

--- client/src/hooks/useFindBackgroundComponent.js
+++
@@ -1,8 +0,0 @@
-import { useLocation } from "react-router-dom"
-import { COMPONENTS } from "../constants/components";
-
-export const useFindBackgroundComponent = () => {
-    const location = useLocation();
-    const backgroundComponent = COMPONENTS[location.state];
-    return backgroundComponent;
-}
