25cff722e54a2d59e9202d3cb938f780d761c018
MaximKraychev <maxim.kraychev@gmail.com>
2023-12-03 19:51:03 +0200

    Remove COMPONENTS constants

--- client/src/constants/components.js
+++
@@ -1,7 +0,0 @@
-import { Explore } from "../components/Explore/Explore";
-import { Profile } from "../components/Profile/Profile";
-
-export const COMPONENTS = {
-    'explore': Explore,
-    'profile': Profile
-}
