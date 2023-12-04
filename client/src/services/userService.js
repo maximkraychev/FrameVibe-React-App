8c6c372af9eefe3e5acdf0944c0b8ff9a8ab1cd7
MaximKraychev <maxim.kraychev@gmail.com>
2023-12-04 12:05:52 +0200

    Splitting getUserInfoByUsernameOrId service to two

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,6 +1,10 @@
 import { API_PATH } from '../constants/paths.js';
 import * as api from './api.js';
 
-export const getUserInfoByUsernameOrId = async (userIdentifier) => {
-    return api.get(API_PATH.USER_FN(userIdentifier));
+export const getUserInfoById = async (userId) => {
+    return api.get(API_PATH.USER_BY_ID_FN(userId));
+};
+
+export const getUserInfoByUsername = async (username) => {
+    return api.get(API_PATH.USER_BY_USERNAME_FN(username));
 };
