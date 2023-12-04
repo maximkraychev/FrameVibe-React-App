c10df7cbd5ffc46f01fda9df95d8bbf0115c28cb
MaximKraychev <maxim.kraychev@gmail.com>
2023-12-04 23:27:51 +0200

    Update Post component (add new logic)

--- client/src/components/Details/Post.jsx
+++ client/src/components/Details/Post.jsx
@@ -1,32 +1,11 @@
-import { useContext } from "react";
-
-import { StateContext } from "../../contexts/StateContext";
-import { STATE_FIELDS } from "../../constants/stateFieldsConstants";
-
 import styles from './Post.module.css';
 import { Details } from "./Details";
-import { Modal } from "../Modal/Modal";
 
 export const Post = () => {
-    const { state } = useContext(StateContext);
 
     return (
-        <>
-            {state[STATE_FIELDS.COMPONENT] &&
-                <>
-                    <Modal>
-                        <Details />
-                    </Modal>
-                    {/* TODO there is a problem calling the component like this after the StrictMode is removed */}
-                    {state[STATE_FIELDS.COMPONENT]()}      
-                </>
-            }
-
-            {!state[STATE_FIELDS.COMPONENT] &&
-                <div className={styles['post-container']}>
-                    <Details />
-                </div>
-            }
-        </>
+        <div className={styles['post-container']}>
+            <Details />
+        </div>
     );
 }
