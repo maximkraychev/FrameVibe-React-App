a835cdf144055f5fb93031cf4e428a2478979557
MaximKraychev <maxim.kraychev@gmail.com>
2023-12-04 23:26:51 +0200

    Create PostWithModal rapper component

---
+++ client/src/components/Details/PostWithModal.jsx
@@ -0,0 +1,10 @@
+import { Modal } from "../Modal/Modal";
+import { Details } from "./Details";
+
+export const PostWithModal = () => {
+    return (
+        <Modal>
+            <Details />
+        </Modal>
+    )
+};
