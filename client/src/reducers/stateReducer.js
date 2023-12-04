0a7029cc86b624e2593b8375e65c0fdcff41134c
MaximKraychev <maxim.kraychev@gmail.com>
2023-12-04 23:23:00 +0200

    Updated StateReducer(removed and added new fields)

--- client/src/reducers/stateReducer.js
+++ client/src/reducers/stateReducer.js
@@ -4,10 +4,10 @@ export const stateReducer = (currentState, action) => {
 
     switch (action?.type) {
         case STATE_FIELDS.POSTS:
-            return { ...currentState, [action.type]: [...action.value] }
-        case STATE_FIELDS.MODAL:
             return { ...currentState, [action.type]: action.value }
-        case STATE_FIELDS.COMPONENT:
+        case STATE_FIELDS.DETAILS_VISIBILITY:
+            return { ...currentState, [action.type]: action.value }
+        case STATE_FIELDS.DETAIL_POST:
             return { ...currentState, [action.type]: action.value }
         default:
             return { ...currentState }
