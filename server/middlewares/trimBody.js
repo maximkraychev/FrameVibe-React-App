2eed9a5e8ea7f36f6f33558ab87d425667074c5b
MaximKraychev <maxim.kraychev@gmail.com>
2023-12-05 21:05:52 +0200

    Add middleware for trimming the inputs in body

---
+++ server/middlewares/trimBody.js
@@ -0,0 +1,8 @@
+export const trimBody = () => (req, res, next) => {
+    if (req.body) {
+        for (const key in req.body) {
+            req.body[key] = req.body[key].trim();
+        }
+    }
+    next();
+};
