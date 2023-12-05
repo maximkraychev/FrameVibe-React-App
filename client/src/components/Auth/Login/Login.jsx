f30ed3110e236f32967b433e23c5ba7a15542558
MaximKraychev <maxim.kraychev@gmail.com>
2023-12-05 16:49:42 +0200

    Add new logic for Login component

--- client/src/components/Auth/Login/Login.jsx
+++ client/src/components/Auth/Login/Login.jsx
@@ -1,29 +1,62 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
-import styles from './Login.module.css';
-import { PATH } from '../../../constants/paths';
-import { INPUT_NAMES } from '../../../constants/formInputNaming';
-
-import { useForm } from '../../../hooks/useForm';
 import { AuthContext } from '../../../contexts/AuthContext';
+import { useFormValidation } from '../../../hooks/useFormValidation';
+import { useForm } from '../../../hooks/useForm';
+import { submitBtnStateCheck } from '../../../util/submitBtnStateCheck';
+import { LOGIN_FORM_VALIDATIONS } from '../../../util/formValidations';
+import { INPUT_NAMES } from '../../../constants/formInputNaming';
+import { PATH } from '../../../constants/paths';
 
+import styles from './Login.module.css';
+import { SubmitBtn } from '../../SubmitBtn/SubmitBtn';
 
+const initialValues = {
+    [INPUT_NAMES.EMAIL]: '',
+    [INPUT_NAMES.PASSWORD]: '',
+}
 
 export const Login = () => {
 
     const { login } = useContext(AuthContext);
     const navigate = useNavigate();
-    const { values, changeHandler, onSubmit } = useForm({
-        [INPUT_NAMES.EMAIL]: '',
-        [INPUT_NAMES.PASSWORD]: '',
-    },
-        onLoginSubmit)
+    const { values, changeHandler, onSubmit } = useForm(initialValues, onLoginSubmit);
+    const { errorMessages, checkFieldForError } = useFormValidation(initialValues, LOGIN_FORM_VALIDATIONS);
+    const [submitButtonState, setSubmitButtonState] = useState(submitBtnStateCheck(values, errorMessages));
+
+    let timeout = [];
 
     async function onLoginSubmit(data) {
         await login(data);
         navigate(PATH.EXPLORE);
         //TODO handle error 
+    }
+
+    function onInputChange(e) {
+        changeHandler(e);
+
+
+        // Set up timeout so the function below does not trigger unless the user have stop typing for 0.5sec
+        // console.log(timeout);
+        // if (timeout.length > 0) {
+        //     timeout.forEach(x => clearTimeout(x));
+        // }
+
+        // timeout = setTimeout(() => {
+            if (errorMessages[e.target.name]) {
+                errorCheck(e);
+            }
+
+            
+        // }, 500)
+
+    }
+
+    function errorCheck(e) {
+        // TODO find a way to use values from useForm (If i try here there is delay until the values are changed)
+        checkFieldForError(e.target.name, e.target.value);
+        setSubmitButtonState(submitBtnStateCheck(values, errorMessages));
     }
 
     return (
@@ -36,7 +69,8 @@ export const Login = () => {
                     placeholder='Email'
                     name={INPUT_NAMES.EMAIL}
                     value={values[INPUT_NAMES.EMAIL]}
-                    onChange={changeHandler}
+                    onChange={onInputChange}
+                    onBlur={errorCheck}
                 />
 
                 <input
@@ -44,10 +78,15 @@ export const Login = () => {
                     placeholder='Password'
                     name={INPUT_NAMES.PASSWORD}
                     value={values[INPUT_NAMES.PASSWORD]}
-                    onChange={changeHandler}
+                    onChange={onInputChange}
+                    onBlur={errorCheck}
                 />
 
-                <input type="submit" value={'Sign in'} />
+                {errorMessages[INPUT_NAMES.EMAIL] && <h2>Error email</h2>}
+                {errorMessages[INPUT_NAMES.PASSWORD] && <h2>Error password</h2>}
+
+                {/* <input type="submit" value={'Sign in'} /> */}
+                <SubmitBtn value={'Sign in'} active={submitButtonState} />
                 <p className={styles['option']}>
                     You don&apos;t have an account? <Link to={PATH.REGISTER}>Register here!</Link>
                 </p>
