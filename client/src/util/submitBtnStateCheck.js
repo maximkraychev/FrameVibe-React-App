f542320c1c09248db9324cd310099a2370d028a4
MaximKraychev <maxim.kraychev@gmail.com>
2023-12-05 18:46:11 +0200

    Remove logs from submitBtnStateCheck

--- client/src/util/submitBtnStateCheck.js
+++ client/src/util/submitBtnStateCheck.js
@@ -4,10 +4,8 @@ export const submitBtnStateCheck = (inputValues, errorValues) => {
 
     if (isInputValuesEmpty == false && isThereAnErrorInInputs == false) {
         // If we don't have an empty string and we don't have error activate submit button 
-        console.log(true);
         return true;
     } else {
-        console.log(false);
         return false;
     }
 };
