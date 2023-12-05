233036bba053f95c1818d2b4e01ec428a4ad71be
MaximKraychev <maxim.kraychev@gmail.com>
2023-12-05 16:47:27 +0200

    Change in formInputNaming constants

--- client/src/constants/formInputNaming.js
+++ client/src/constants/formInputNaming.js
@@ -5,6 +5,9 @@ export const INPUT_NAMES = {
     REPASSWORD: 'repassword',
     DESCRIPTION: 'description',
     UPLOAD_IMAGE: 'uploadImage',
-    USER_BASE_AVATAR_URL: 'https://res.cloudinary.com/framevibe/image/upload/v1701085833/xjk2rcmagmbyxfuk9vfz.png',
     USER_AVATAR: 'avatar'
+}
+
+export const INPUT_BASE = {
+    USER_BASE_AVATAR_URL: 'https://res.cloudinary.com/framevibe/image/upload/v1701085833/xjk2rcmagmbyxfuk9vfz.png'
 }
