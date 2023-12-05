cf378c86abdce82eae027cf8130e4fe2c5c00ff4
MaximKraychev <maxim.kraychev@gmail.com>
2023-12-05 23:29:51 +0200

    Add more Form validation

--- client/src/util/formValidations.js
+++ client/src/util/formValidations.js
@@ -8,3 +8,28 @@ export const LOGIN_FORM_VALIDATIONS = {
         (value) => value !== '' ? null : 'Password is required!'
     ]
 }
+
+export const UPLOAD_FORM_VALIDATION = {
+    [INPUT_NAMES.UPLOAD_IMAGE]: [
+        (value) => value !== '' ? null : 'Image is required!'
+    ],
+    [INPUT_NAMES.DESCRIPTION]: [
+        (value) => value !== '' ? null : 'Description is required!'
+    ]
+}
+
+export const REGISTER_FORM_VALIDATIONS = {
+    [INPUT_NAMES.EMAIL]: [
+        (value) => value !== '' ? null : 'Email is required!'
+    ],
+    [INPUT_NAMES.USERNAME]: [
+        (value) => value !== '' ? null : 'Username is required!'
+    ],
+    [INPUT_NAMES.PASSWORD]: [
+        (value) => value !== '' ? null : 'Password is required!',
+        (value) => value.length >= 6 ? null : 'Password must be at least six characters!'
+    ],
+    [INPUT_NAMES.REPASSWORD]: [
+        (value) => value !== '' ? null : 'RePassword is required!'
+    ],
+}
