a518b941000d9975a6cdece418a04032ef5ef25b
MaximKraychev <maxim.kraychev@gmail.com>
2023-12-06 12:12:37 +0200

    Add comments with instruction (to fix SubmitBtn)

--- client/src/util/submitBtnStateCheck.js
+++ client/src/util/submitBtnStateCheck.js
@@ -1,3 +1,6 @@
+//TODO... to maybe make it better make it a hook, save the SubmitBtn state here and on init accept a third parameter(option)
+// that will include a input names that can be excluded from the check for empty string(that will be the valid fields that can be empty)
+
 export const submitBtnStateCheck = (inputValues, errorValues) => {
     const isInputValuesEmpty = Object.values(inputValues).some(value => value == '');
     const isThereAnErrorInInputs = Object.values(errorValues).some(value => value !== null);
