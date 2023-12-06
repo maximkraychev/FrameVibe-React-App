45b262439d60e5c1fac35feb17431e46437b909c
MaximKraychev <maxim.kraychev@gmail.com>
2023-12-06 23:54:16 +0200

    Add function that stop propagation

--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -4,10 +4,19 @@ import styles from './Modal.module.css';
 export const Modal = ({ children }) => {
     const { closeHandlerDetailsModal } = useDetailsModal();
 
+    // !!!
+    // This function execute only if the dimmer div is clicked 
+    // That way the clicks inside child component after bubbling doesn't close the modal
+    function closeTheModal(e) {
+
+        if (e.target == e.currentTarget) {
+            closeHandlerDetailsModal()
+        }
+    }
 
     return (
-        <div className={styles['dimmer']} onClick={closeHandlerDetailsModal}>
-            <div className={styles['modal-container']}>
+        <div className={styles['dimmer']} onClick={closeTheModal}>
+            <div className={styles['modal-container']} >
                 {children}
             </div>
         </div>
