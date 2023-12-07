0f565b1b8f73d08fbed4db19394ca3d91289079e
MaximKraychev <maxim.kraychev@gmail.com>
2023-12-08 00:25:32 +0200

    Changes to Modal component and css

--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -1,33 +1,33 @@
+import { useContext } from 'react';
+
+import { StateContext } from '../../contexts/StateContext';
+import { STATE_FIELDS } from '../../constants/stateFieldsConstants';
+
 import styles from './Modal.module.css';
 
-export const Modal = ({ children, closeModal }) => {
+export const Modal = ({ children }) => {
+
+    const { state, changeModalState } = useContext(StateContext);
 
     // !!!
     // This function execute only if the dimmer div is clicked 
     // That way the clicks inside child component after bubbling doesn't close the modal
     function closeTheModal(e) {
-      
-        if (e.target == e.currentTarget) {
 
-            // In case the modal is used to show post details
-            // From props we should have function closeModal
-            // Execute closeModal to close the modal
-            if (closeModal) {
-                closeModal();
-                return;
-            }
-           
-            // In general use to close the modal 
-            // Execute the function that will change the global state
-            closeHandlerDetailsModal()
+        if (e.target == e.currentTarget) {
+            changeModalState(false);
         }
     }
 
     return (
-        <div className={styles['dimmer']} onClick={closeTheModal}>
-            <div className={styles['modal-container']} >
-                {children}
-            </div>
-        </div>
+        <>
+            {state[STATE_FIELDS.MODAL] &&
+                <div className={styles['dimmer']} onClick={closeTheModal}>
+                    <div className={styles['modal-container']} >
+                        {children}
+                    </div>
+                </div>
+            }
+        </>
     );
 }
