43b26a6656d682b4cedfa78da9c0155748095a7a
MaximKraychev <maxim.kraychev@gmail.com>
2023-12-08 00:25:56 +0200

    Changes to PostWithModal component

--- client/src/components/Details/PostWithModal.jsx
+++ client/src/components/Details/PostWithModal.jsx
@@ -1,18 +1,29 @@
 import { useNavigate } from "react-router-dom";
-import { Modal } from "../Modal/Modal";
+
+import styles from './PostWithModal.module.css';
 import { Details } from "./Details";
 
 export const PostWithModal = () => {
 
-    const navigation = useNavigate()
+    const navigation = useNavigate();
 
-    function closePostModal () {
-        navigation(-1);
+    // !!!
+    // This function execute only if the dimmer div is clicked 
+    // That way the clicks inside child component after bubbling doesn't close the modal
+    function closePostModal(e) {
+        if (e.target == e.currentTarget) {
+            navigation(-1);
+        }
     }
 
     return (
-        <Modal closeModal={closePostModal}>
-            <Details />
-        </Modal>
+
+        <div className={styles['dimmer-post-modal']} onClick={closePostModal}>
+            <div className={styles['modal-container']} >
+                <Details />
+            </div>
+        </div>
+
+
     )
 };
