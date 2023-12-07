d2b716e375e727cd6c53827b3f95202b6ffb026c
MaximKraychev <maxim.kraychev@gmail.com>
2023-12-07 20:01:36 +0200

    Updated AuthGuard (remove redirection if params)

--- client/src/guards/AuthGuard.jsx
+++ client/src/guards/AuthGuard.jsx
@@ -1,18 +1,13 @@
 import { useContext } from "react"
-import { Navigate, Outlet, useParams } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
 import { AuthContext } from "../contexts/AuthContext"
-import { PARAMS, PATH } from "../constants/paths";
+import { PATH } from "../constants/paths";
 
 export const AuthGuard = () => {
     const { auth } = useContext(AuthContext);
-    const params = useParams();
-    const postId = params[PARAMS.POSTID]
 
     if (!auth) {
-        if (postId) {
-            return <Navigate to={PATH.POST_FN(postId)} />
-        }
         return <Navigate to={PATH.LOGIN} />;
     }
 
