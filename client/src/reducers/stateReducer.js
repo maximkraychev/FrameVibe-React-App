d575dcc1b8b0b0796addf327445df21e6350f754
MaximKraychev <maxim.kraychev@gmail.com>
2023-12-07 17:19:05 +0200

    Update state reducer

--- client/src/reducers/stateReducer.js
+++ client/src/reducers/stateReducer.js
@@ -3,11 +3,13 @@ import { STATE_FIELDS } from '../constants/stateFieldsConstants';
 export const stateReducer = (currentState, action) => {
 
     switch (action?.type) {
-        case STATE_FIELDS.POSTS:
+        case STATE_FIELDS.POSTS_EXPLORE:
+            return { ...currentState, [action.type]: action.value }
+        case STATE_FIELDS.POSTS_PROFILE:
             return { ...currentState, [action.type]: action.value }
         case STATE_FIELDS.DETAILS_VISIBILITY:
             return { ...currentState, [action.type]: action.value }
-        case STATE_FIELDS.DETAIL_POST:
+        case STATE_FIELDS.POST_MODAL:
             return { ...currentState, [action.type]: action.value }
         default:
             return { ...currentState }
