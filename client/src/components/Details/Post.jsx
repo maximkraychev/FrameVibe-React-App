9f107d690dfb4882f30dba015caa34f23c1a1015
MaximKraychev <maxim.kraychev@gmail.com>
2023-12-08 00:41:51 +0200

    Change css for Share component and Details

--- client/src/components/Details/Post.jsx
+++ client/src/components/Details/Post.jsx
@@ -3,10 +3,6 @@ import { Details } from "./Details";
 
 export const Post = () => {
 
-    // async function test () {
-    //     await navigator.clipboard.writeText('asd')
-    // }
-
     return (
         <div className={styles['post-container']}>
             <Details />
