d75f4e95608208ba978b6d1ce3b16f3f389e0d6d
MaximKraychev <maxim.kraychev@gmail.com>
2023-12-08 22:36:41 +0200

    Add like and dislike functionality to PostCard component

--- client/src/components/Explore/PostCard/PostCard.jsx
+++ client/src/components/Explore/PostCard/PostCard.jsx
@@ -1,10 +1,55 @@
+import { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { HeartSvg } from '../../Svg/Heart';
+
+import { AuthContext } from '../../../contexts/AuthContext';
+import { dislikePost, likePost } from '../../../services/postService';
+
 import styles from './PostCard.module.css';
+import { HeartSvg } from '../../Svg/Heart';
+import { HeartSolidSvg } from '../../Svg/HeartSolid';
+import { useSyncStateWithNewPost } from '../../../hooks/useSyncStateWithNewPost';
 
 export const PostCard = ({ post }) => {
 
+    const { auth } = useContext(AuthContext);
+    const [isLiked, setIsLiked] = useState(() => post.likes.includes(auth._id));
+    const { syncState } = useSyncStateWithNewPost();
+
     const { owner, imageURL, description, likes, _id } = post;
+
+
+    async function onLike() {
+
+        try {
+            // If owner return
+            if ((auth && auth._id === post?.owner?._id)) return;
+
+            const updatedPost = await likePost(_id);
+
+            setIsLiked(true);
+            syncState(updatedPost);
+        } catch (err) {
+            console.log(err);
+            //TODO handle the error;
+        }
+
+    }
+
+    async function onDisLike() {
+        try {
+            // If owner return
+            if ((auth && auth._id === post?.owner?._id)) return;
+
+            const updatedPost = await dislikePost(_id);
+
+            setIsLiked(false);
+            syncState(updatedPost);
+        } catch (err) {
+            console.log(err);
+            //TODO handle the error;
+        }
+    }
+
 
     return (
         <>
@@ -24,8 +69,22 @@ export const PostCard = ({ post }) => {
                     </Link>
                 </div>
                 <div className={styles['actions']}>
-                    <HeartSvg />
+
+                    {isLiked
+                        ? <span
+                            className={styles['svg-container']}
+                            onClick={onDisLike}>
+                            <HeartSolidSvg />
+                        </span>
+                        : <span
+                            className={styles['svg-container']}
+                            onClick={onLike}>
+                            <HeartSvg />
+                        </span>
+                    }
+
                     <p>{likes?.length} likes</p>
+
                 </div>
                 <p className={styles['description']}>
                     {description}
