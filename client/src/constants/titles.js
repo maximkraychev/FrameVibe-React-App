a049c1b90cb9707f7cc6e950d6cc0cad7be23e01
MaximKraychev <maxim.kraychev@gmail.com>
2023-12-08 23:28:49 +0200

    Add constant with titles

---
+++ client/src/constants/titles.js
@@ -0,0 +1,10 @@
+export const SITE_TITLE = {
+    LOGIN: 'Login',
+    REGISTER: 'Register',
+    CREATE_POST: 'Create Post',
+    PROFILE_FN: (username) => `${username} profile`,
+    EDIT_POST: 'Edit Post',
+    DETAILS: 'Post Details',
+    EXPLORE: 'Explore',
+    NOT_FOUND: 'Page Not Found'
+}
