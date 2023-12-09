70e46e1344e8378bfd81e8552377ec221bbd45f8
MaximKraychev <maxim.kraychev@gmail.com>
2023-12-09 20:05:40 +0200

    Add vercel.json

--- server/config/database.js
+++ server/config/database.js
@@ -5,11 +5,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // Get environment variable for connection string
-const CONNECTION_STRING = process.env.CONNECTION_STRING;
+const CONNECTION_STRING_MONGODB = process.env.CONNECTION_STRING_MONGODB;
 
 export default async () => {
 	try {
-		await connect(CONNECTION_STRING);
+		await connect(CONNECTION_STRING_MONGODB);
 		console.log('Database successfully connected');
 	} catch (error) {
 		console.error('Error initializing database');
