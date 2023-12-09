b636beb12b6008b2ef2b82881574865cff0a4c53
MaximKraychev <maxim.kraychev@gmail.com>
2023-12-09 21:27:21 +0200

    Set Access-Control-Allow-Origin

--- server/middlewares/cors.js
+++ server/middlewares/cors.js
@@ -1,6 +1,6 @@
 export default () => (req, res, next) => {
     // Here we can add which host can use our server
-	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Origin', 'https://frame-vibe-react-app.vercel.app');
 
 	res.setHeader(
 		'Access-Control-Allow-Methods',
